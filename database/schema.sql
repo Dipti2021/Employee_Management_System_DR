b1ec0412f5a8b65ed16cd269d1a3119f7cca5b18
Dipti2021 <77368913+Dipti2021@users.noreply.github.com>
2021-05-16 17:05:14 -0400

    Added On Delete Cascade

--- database/schema.sql
+++ database/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE emprole (-- roles
  title  VARCHAR(100) NOT NULL, -- to hold role title
  salary  DECIMAL(10,2) NULL, -- to hold role salary     
  department_id INTEGER NOT NULL, -- to hold reference to department role belongs to
- FOREIGN KEY (department_id) REFERENCES department(id), 
+ FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE, // to undo the foreign key
  PRIMARY KEY (id)
 );
 
@@ -24,8 +24,8 @@ CREATE TABLE employee(
  last_name  VARCHAR(100) NOT NULL, -- to hold employee last name
  role_id INTEGER NOT NULL, -- to hold reference to role employee has
  manager_id INTEGER NOT NULL, -- to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
- FOREIGN KEY (role_id) REFERENCES emprole(id),-- holds the foreign key for the role id
- FOREIGN KEY (manager_id) REFERENCES employee(id),-- holds the foreign key for the manager id
+FOREIGN KEY (role_id) REFERENCES emprole(id) ON DELETE CASCADE,-- holds the foreign key for the role id
+ FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,-- holds the foreign key for the manager id
  PRIMARY KEY (id)
 );
 
